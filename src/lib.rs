e53313dbbdb6ecc9d4e3253169690e0803984b0f
mosaic <mosaic@hotmail.com>
2026-01-28 09:33:35 +0800

    extend multiple_tables

--- src/lib.rs
+++ src/lib.rs
@@ -948,55 +948,6 @@ impl RemDb {
                 }
             };
 
-            // 读取表名长度
-            let mut table_name_len_bytes = [0u8; 1];
-            let read = crate::platform::file_read(
-                handle,
-                table_name_len_bytes.as_mut_ptr(),
-                table_name_len_bytes.len(),
-            )
-            .map_err(|_| RemDbError::FileIoError)?;
-            if read != table_name_len_bytes.len() {
-                return Err(RemDbError::FileIoError);
-            }
-            let table_name_len = table_name_len_bytes[0] as usize;
-            
-            // 读取表名
-            let mut table_name = vec![0u8; table_name_len];
-            let read = crate::platform::file_read(
-                handle,
-                table_name.as_mut_ptr(),
-                table_name.len(),
-            )
-            .map_err(|_| RemDbError::FileIoError)?;
-            if read != table_name.len() {
-                return Err(RemDbError::FileIoError);
-            }
-            
-            // 读取字段数量
-            let mut field_count_bytes = [0u8; 1];
-            let read = crate::platform::file_read(
-                handle,
-                field_count_bytes.as_mut_ptr(),
-                field_count_bytes.len(),
-            )
-            .map_err(|_| RemDbError::FileIoError)?;
-            if read != field_count_bytes.len() {
-                return Err(RemDbError::FileIoError);
-            }
-            
-            // 读取主键字段数量
-            let mut primary_key_count_bytes = [0u8; 1];
-            let read = crate::platform::file_read(
-                handle,
-                primary_key_count_bytes.as_mut_ptr(),
-                primary_key_count_bytes.len(),
-            )
-            .map_err(|_| RemDbError::FileIoError)?;
-            if read != primary_key_count_bytes.len() {
-                return Err(RemDbError::FileIoError);
-            }
-            
             // 读取记录数
             let mut record_count_bytes = [0u8; 4];
             let read = crate::platform::file_read(
